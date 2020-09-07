67eb8a6ba5251bc7e95fd4a3186beea85939a096
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-09-08 00:32:03 +0300

    Updates

---
+++ precache-manifest.d9d8d021cbc59e47864961c1cfbd9f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c6efb60ddfba4a4b078f15c055911b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1df8a395decd090c7170",
+    "url": "./static/css/main.79a10ac7.chunk.css"
+  },
+  {
+    "revision": "28d99ea5ca63a2d1b406",
+    "url": "./static/js/2.bf829fdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.bf829fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df8a395decd090c7170",
+    "url": "./static/js/main.319f281f.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.d9d8d021cbc59e47864961c1cfbd9f9f.js"
 );
 
 self.addEventListener('message', (event) => {
